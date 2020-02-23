d9d221d7c31e07eab1ffe50c5620c8f256155876
Corbin McElhanney <corbin.mcelhanney@gmail.com>
2020-02-23 08:22:31 -0500

    Sunday morning

--- direction_control/direction_control.ino
+++ direction_control/direction_control.ino
@@ -7,15 +7,14 @@
 
 typedef struct {
   byte dirpin, steppin;
-  int range;
+  int steprange, stepsep;
+  int wind_dir, unwind_dir;
 } Motor;
 Motor motors[NUM_MOTORS];
 
 // int numberOfSteps = 100;
 byte ledPin = LED_BUILTIN;
 int pulseWidthMicros = 20;  // microseconds
-int millisbetweenSteps = 10; // milliseconds
-
 
 void step(Motor m, int steps) {
   // Turn on the LED while stepping
@@ -23,9 +22,9 @@ void step(Motor m, int steps) {
 
   if (steps < 0) {
     steps = -steps;
-    digitalWrite(m.dirpin, UNWIND);
+    digitalWrite(m.dirpin, m.unwind_dir);
   } else {
-    digitalWrite(m.dirpin, WIND);
+    digitalWrite(m.dirpin, m.wind_dir);
   }
   
   for(int n = 0; n < steps; n++) {
@@ -33,61 +32,95 @@ void step(Motor m, int steps) {
     delayMicroseconds(pulseWidthMicros);
     digitalWrite(m.steppin, LOW);
    
-    delay(millisbetweenSteps);
+    delayMicroseconds(m.stepsep);
   }
  
   digitalWrite(ledPin, LOW);
 }
 
+void stepBoth(int leftSteps, int rightSteps, bool slow) {
+  // Turn on the LED while stepping
+  digitalWrite(ledPin, HIGH);
 
-void calibrateMotors() {
-  Serial.println("Starting motor configuration...");
-
-  for (int i = 0; i < NUM_MOTORS; ++i) {
-    Serial.print("Stretch motor ");
-    Serial.print(i);
-    Serial.println(" as tight as possible");
-    Serial.println("Type 0 when done");
-
-    int in;
-    int stretched = 0;
-    do {
-      in = Serial.parseInt();
-      stretched += in;
-      step(motors[i], in);
-    } while (in != 0);
-
-    Serial.print("Loosen motor ");
-    Serial.print(i);
-    Serial.println(" so it just loses tension");
-    Serial.println("Type 0 when done");
-
-    int loosened = 0;
-    do {
-      in = Serial.parseInt();
-      loosened += in;
-      step(motors[i], in);
-    } while (in != 0);
-
-    motors[i].range = -loosened;
+  if (leftSteps < 0) {
+    leftSteps = -leftSteps;
+    digitalWrite(motors[0].dirpin, motors[0].unwind_dir);
+  } else {
+    digitalWrite(motors[0].dirpin, motors[0].wind_dir);
   }
+
+  if (rightSteps < 0) {
+    rightSteps = -rightSteps;
+    digitalWrite(motors[1].dirpin, motors[1].unwind_dir);
+  } else {
+    digitalWrite(motors[1].dirpin, motors[1].wind_dir);
+  }
+
+  int leftStepsep = !slow ? motors[0].stepsep / 100 : 200;
+  int rightStepsep = !slow ? motors[1].stepsep / 100 : 200;
+  int leftTotal = leftSteps*leftStepsep;
+  int rightTotal = rightSteps*rightStepsep;
+  
+  for(int n = 0; n < max(leftTotal, rightTotal); n++) {
+    if (n % leftStepsep == 0 && n < leftTotal) {
+      digitalWrite(motors[0].steppin, HIGH);
+      delayMicroseconds(pulseWidthMicros);
+      digitalWrite(motors[0].steppin, LOW);
+    }
+    if (n % rightStepsep == 0 && n < rightTotal) {
+      digitalWrite(motors[1].steppin, HIGH);
+      delayMicroseconds(pulseWidthMicros);
+      digitalWrite(motors[1].steppin, LOW);
+    }
+   
+    delayMicroseconds(100);
+  }
+ 
+  digitalWrite(ledPin, LOW);
 }
 
 void dodgeAngle(int angle) {
-  if (angle >= 270 && angle <= 360) {
-    step(motors[0], motors[0].range);
-    delay(2000);
-    step(motors[0], -motors[0].range);
+  // rotate angle frame by 45 degrees
+  angle += 45;
+  if (angle >= 360) {
+    angle -= 360;
   }
-}
 
+  double sintheta = sin(angle * (PI / 180.0));
+  double costheta = cos(angle * (PI / 180.0));
+  
+  // calculate motor offsets
+  double left = sintheta * motors[0].steprange / 2;
+  double right = costheta * motors[1].steprange / 2;
+
+  stepBoth(left, right, false);
+//  step(motors[0], left);
+//  step(motors[1], right);
+
+  delay(2000);
+
+  stepBoth(-left, -right, true);
+//
+//  step(motors[0], -left);
+//  step(motors[1], -right);
+}
 
 void setup()
 {
   motors[0].dirpin = 2;
   motors[0].steppin = 4;
+  motors[0].wind_dir = WIND;
+  motors[0].unwind_dir = UNWIND;
   motors[1].dirpin = 12;
   motors[1].steppin = 13;
+  motors[1].wind_dir = UNWIND;
+  motors[1].unwind_dir = WIND;
+
+  // motor config
+  motors[0].steprange = 200;
+  motors[0].stepsep = 1500;
+  motors[1].steprange = 260;
+  motors[1].stepsep = 1200;
   
   Serial.begin(9600);
   Serial.println("Starting StepperTest");
@@ -102,17 +135,19 @@ void setup()
   pinMode(7, OUTPUT);
   digitalWrite(7, HIGH);
 
-  Serial.println("Type -1 to calibrate or 0-365 to avoid");
+  // Centre pinata
+  stepBoth(motors[0].steprange / 2, motors[1].steprange / 2, true);
+//  step(motors[0], motors[0].steprange / 2);
+//  step(motors[1], motors[1].steprange / 2);
 }
 
 void loop()
 {
   while (Serial.available() > 0) {
     int angle = Serial.parseInt();
+    Serial.println(angle);
 
-    if (angle == -1) {
-      calibrateMotors();
-    } else {
+    if (angle > 0 && angle < 360) {
       dodgeAngle(angle);
     }
   }
